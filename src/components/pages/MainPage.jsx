1a66f97b0686678050a94355d153926a01c6a202
Milksider <pashagachiman@mail.ru>
2023-04-29 23:40:10 +0300

    added last sections to main page

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -452,7 +452,139 @@ function MainPage(){
     </section>
     <section className="price">
         <div className="main-container">
-            
+            <div className="circle-mid">
+                        <img src="./images/Circle.svg" alt=""/>
+                        <p>Прайс-лист</p>
+            </div>
+            <h1 className="title-mid"> </h1>
+            <div className="price__content">
+                <div className="price__list">
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Landing-page</h3>
+                                <h3>от 8000 руб.</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Одностаничный сайт для презентации услуг/товаров/...
+                            </p>
+                        </a>
+                    </div>
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Корпоративный сайт</h3>
+                                <h3>от 20000 руб.</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Сайт для вашего бизнеса или компании
+                            </p>
+                        </a>
+                    </div>
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Интернет магазин</h3>
+                                <h3>от 30000 руб.</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Инструмент для ваших продаж в сети
+                            </p>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="price__list">
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Web-приложение</h3>
+                                <h3>от 28000 руб.</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Приложение внутри браузера для лучшего комфорта
+                            </p>
+                        </a>
+                    </div>
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Android/IOS Приложение</h3>
+                                <h3>от 25000 руб.</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Приложение для смартфона любой ОС
+                            </p>
+                        </a>
+                    </div>
+                    <div className="price__item">
+                        <img src="./images/Circle.svg" alt="" className="price__item-img" />
+                        <a href="#" className="price__item-link">
+                            <div className="price__item-title">
+                                <h3>Дизайн</h3>
+                                <h3>Индивудуально</h3>
+                            </div>
+                            <p className="price__item-text">
+                                Создам дизайн для вашего сайт/приложения...
+                            </p>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section className="feedback">
+        <div className="main-container">
+            <div className="feedback__inner">
+                <div className="fedback__picture">
+                    <img className="feedback__picture-img" src="./images/me.jpg" alt="" />
+                </div>
+                <div className="feedback__content">
+                    <div className="circle-left">
+                        <img src="./images/Circle.svg" alt=""/>
+                        <p>Контакты</p>
+                    </div>
+                    <h1 className="title-left">Свяжитесь со мной</h1>
+                    <h3>Любым удобным для вас способом:</h3>
+                    <div className="feedback__links">
+                        <a href="#" className="feedback__link">
+                            <img src="./images/Instagram-2.svg" alt="" />
+                        </a>
+                        <a href="#" className="feedback__link">
+                            <img src="./images/Instagram-2.svg" alt="" />
+                        </a>
+                        <a href="#" className="feedback__link">
+                            <img src="./images/Instagram-2.svg" alt="" />
+                        </a>
+                    </div>
+                    <h3>Или оставьте ваши данные и я сама вам напишу:</h3>
+                    <form action="#">
+                        <div className="form__input-pair">
+                            <div className="form__input-block">
+                                <label className="feedback__form-label" htmlFor="">Ваше имя</label>
+                                <input className="feedback__form-name" type="text" name="name" id="" placeholder="Иван" autoComplete="name"/>
+                            </div>
+                            <div className="form__input-block">
+                                <label className="feedback__form-label" htmlFor="">Ваша почта</label>
+                                <input className="feedback__form-mail" type="email" name="mail" id="" autoComplete="email"/>
+                            </div>
+                        </div>
+                        <div className="form__input-block">
+                            <label htmlFor="" className="feedback__form-label">Ваше сообщение</label>
+                            <textarea className="feedback__form-text" name="textarea" id=""></textarea>
+                        </div>
+                        <button type="submit" className="feedback__form-btn">
+                            Отправить сообщение
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
         </>
